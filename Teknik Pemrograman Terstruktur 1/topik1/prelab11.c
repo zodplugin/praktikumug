dc0036a25efc99dc2745352cab976f080425c108
Muhammad Fadhlan Aqila <60656929+zodplugin@users.noreply.github.com>
2021-10-26 10:43:57 +0700

    Create prelab11.c

---
+++ Teknik Pemrograman Terstruktur 1/topik1/prelab11.c
@@ -0,0 +1,11 @@
+// Program mencetak kalimat
+#include <stdio.h>
+
+// Fungsi main untuk memulai eksekusi program
+int main()
+{ // Awal dari body program​
+    // Mencetak output
+	printf("This is C program." ); 
+	
+	return 0; // Menentukan nilai balik
+} // Akhir dari body program
