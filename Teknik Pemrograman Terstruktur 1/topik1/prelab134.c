f353ab3d81689c0d3bfb26ac302e2f0bf809dbd3
Muhammad Fadhlan Aqila <60656929+zodplugin@users.noreply.github.com>
2021-10-26 11:01:02 +0700

    Create prelab134.c

---
+++ Teknik Pemrograman Terstruktur 1/topik1/prelab134.c
@@ -0,0 +1,10 @@
+// Program menggunakan escape sequence
+#include <stdio.h>
+
+int main()
+{
+    // Mencetak output "Buku adalah jendela dunia."
+    printf("\"Buku adalah jendela dunia.\"");
+    
+    return 0;
+}
