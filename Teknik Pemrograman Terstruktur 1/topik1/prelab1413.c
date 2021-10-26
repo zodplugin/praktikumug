a8dcac3f682e6a408154af9e3b6775ab2229372c
Muhammad Fadhlan Aqila <60656929+zodplugin@users.noreply.github.com>
2021-10-26 10:59:06 +0700

    Create prelab1413.c

---
+++ Teknik Pemrograman Terstruktur 1/topik1/prelab1413.c
@@ -0,0 +1,16 @@
+// Program mencetak karakter
+#include <stdio.h>
+
+int main()
+{
+    // Deklarasi variabel Karakter dengan tipe data char
+    char a;
+    
+    // Menugaskan variabel Karakter dengan nilai A
+    a = 'A';
+    
+    // Mencetak output nilai variabel Karakter ke layar
+    printf("Karakter yang disimpan = %c",a);
+
+    return 0;
+}
