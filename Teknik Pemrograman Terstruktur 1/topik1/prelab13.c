2dcb5dc08b21ff993e666637c0de6b8c73ff94e6
Muhammad Fadhlan Aqila <60656929+zodplugin@users.noreply.github.com>
2021-10-26 10:50:40 +0700

    Create prelab13.c

---
+++ Teknik Pemrograman Terstruktur 1/topik1/prelab13.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() 
+{
+    // Mencetak output
+    printf("Hallo, mahasiswa Gunadarma\n");
+    printf("Selamat datang di Integrated Laboratory");
+    
+    return 0; // Menentukan nilai balik
+}
