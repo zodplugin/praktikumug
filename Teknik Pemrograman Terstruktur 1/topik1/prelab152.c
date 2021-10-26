5857b42e0557863a1ab1a699221d5ab8eeda6f8b
Muhammad Fadhlan Aqila <60656929+zodplugin@users.noreply.github.com>
2021-10-27 00:00:48 +0700

    Create prelab152.c

---
+++ Teknik Pemrograman Terstruktur 1/topik1/prelab152.c
@@ -0,0 +1,23 @@
+// Program IniString.c
+#include <stdio.h>
+
+
+// Fungsi main untuk memulai eksekusi program
+int main()
+{
+    // Deklarasi dan inisialisasi variabel character 
+	char character = 'A';
+	
+	// Mencetak output 
+	printf("%c",character);
+	printf("\nIni adalah string\n");
+	
+    // Deklarasi dan inisialisasi variabel string 
+	char string[20] = "string";
+	
+    // Mencetak output
+	printf("Ini juga adalah %s", string);
+	
+	
+    return 0;
+} // Mengakhiri fungsi main()
