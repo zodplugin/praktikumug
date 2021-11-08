27d44ba57bd89de6595f64371214b3362a7589cf
Muhammad Fadhlan Aqila <60656929+zodplugin@users.noreply.github.com>
2021-11-08 17:58:26 +0700

    Create labactivity2-2.c

---
+++ Teknik Pemrograman Terstruktur 1/topik2/labactivity2-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    // Deklarasi dan inisialisasi variabel nilai
+    int nilai = 60 ;
+    
+    // Statement if untuk kondisi nilai lebih besar atau sama dengan 80
+    if (nilai >= 80)
+    {
+        printf("Nilai Anda baik\n"); // Statement printf untuk mencetak output jika kondisi benar
+    }
+    else
+    {
+        printf("Nilai Anda kurang baik\n"); // Statement printf untuk mencetak output jika kondisi salah
+    }
+    
+    return 0;
+} 
