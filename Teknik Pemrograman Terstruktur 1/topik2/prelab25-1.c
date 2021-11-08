3917ccf0159a0a2c15bba6bfff28411f4303070a
Muhammad Fadhlan Aqila <60656929+zodplugin@users.noreply.github.com>
2021-11-08 17:51:41 +0700

    Create prelab25-1.c

---
+++ Teknik Pemrograman Terstruktur 1/topik2/prelab25-1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    // Deklarasi variabel nilai
+    int nilai ;
+    
+    // Inisialisasi variabel nilai
+    nilai = 80;
+    
+    if (nilai <= 80)
+    {
+        printf("Nilai Anda B \n"); // Statement jika kondisi benar
+    }
+    
+    return 0; // Membalikan nilai
+}
