764c01d5b0047fa7b0aa490af8643e812ed87e18
Muhammad Fadhlan Aqila <60656929+zodplugin@users.noreply.github.com>
2021-11-18 23:07:31 +0700

    Update prelab1-1.c

--- Teknik Pemrograman Terstruktur 1/topik3/3.1/prelab1-1.c
+++ Teknik Pemrograman Terstruktur 1/topik3/3.1/prelab1-1.c
@@ -1,24 +1,26 @@
+#include<stdio.h>
 
-#include <stdio.h>
-
-int main(){
-//	int a = 5;
-//    
-//    a++;
-//    printf("Setelah post-increment, a = %d\n", a);
-//    
-//    a--;
-//    printf("Setelah post-decrement, a = %d\n", a);
-//
-//    return 0;	
+int main()
+{
+    // Deklarasi variabel a, b, dan c dengan tipe data int
+    int a,b,c;
+    a = 15;     // Menugaskan a dengan nilai 15
+    b = 5;             // Menugaskan b dengan nilai 5
+    c = a+b;// Hitung variabel c dengan a ditambah dengan b
     
-//    int z, y, x = 5;
-//    y = ++x; // Menugaskan variabel y dengan nilai x pre-increment
-//    z = 10 + y;
-//    printf("%d", z);
+    a++;    // Post-increment variabel a
+    // Sekarang nilai variabel a adalah 16
+    printf("Setelah post-increment, a = %d\n", a); 
+     
+    b--;     // Post-decrement variabel b
+    // Sekarang nilai variabel b adalah 4
+    printf("Setelah post-decrement, b = %d\n", b); 
+    b++;    // Post-increment variabel b, sekrang nilai variabel b adalah 5
+     
+    // Variabel c ditugaskan kembali dengan a + b, sehingga 16 + 5
+    c = a + b;
+    // Mencetak kalimat dan memanggil variabel c
+    printf("Nilai a + b = %d\n", c);
 
-	int beta, alpha = 10;
-	beta = ++alpha * alpha;
-	printf("%d", beta);
     return 0;
 }
