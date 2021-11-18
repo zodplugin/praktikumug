c68720ba9b99da7ffc68847b4eb44ad61b46fb96
Muhammad Fadhlan Aqila <60656929+zodplugin@users.noreply.github.com>
2021-11-18 23:07:16 +0700

    Create prelab1-1.c

---
+++ Teknik Pemrograman Terstruktur 1/topik3/3.1/prelab1-1.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+int main(){
+//	int a = 5;
+//    
+//    a++;
+//    printf("Setelah post-increment, a = %d\n", a);
+//    
+//    a--;
+//    printf("Setelah post-decrement, a = %d\n", a);
+//
+//    return 0;	
+    
+//    int z, y, x = 5;
+//    y = ++x; // Menugaskan variabel y dengan nilai x pre-increment
+//    z = 10 + y;
+//    printf("%d", z);
+
+	int beta, alpha = 10;
+	beta = ++alpha * alpha;
+	printf("%d", beta);
+    return 0;
+}
