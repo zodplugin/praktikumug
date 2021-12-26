5815e71b9a6c6a4da02b7123e33cd71113ea0e49
Muhammad Fadhlan Aqila <60656929+zodplugin@users.noreply.github.com>
2021-12-26 21:03:02 +0700

    Create activity5-3.c

---
+++ Teknik Pemrograman Terstruktur 1/topik5/activity5-3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    // Deklarasi string movie dan beri batasan 14 karakter pada string tersebut
+    char movie[14] = "Jack and jill";
+    
+    // Mencetak string movie
+    printf("%s",movie);
+    return 0; 
+    
+}
