2202a993661869c21a97a37802c9704f9b06a525
zodplugin <60656929+zodplugin@users.noreply.github.com>
2022-04-12 11:49:10 +0700

    Create bukuprelab.java

---
+++ Pemrograman Berbasis Objek/topik 3/bukuprelab.java
@@ -0,0 +1,17 @@
+public class Buku
+{
+    private String judul;
+    private String penulis;
+    private String penerbit;
+    private int jumlahTerjual;
+    
+    // Tuliskan sebuah constructor yang menerima sebuah argument
+    // untuk setiap field di bawah!
+    public Buku(String judul,String penulis,String penerbit,int jumlahTerjual){
+        this.judul = judul;
+        this.penulis = penulis;
+        this.penerbit = penerbit;
+        this.jumlahTerjual = jumlahTerjual;
+    }
+    
+}
