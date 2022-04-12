d13ae36bed815106e85f2d48e85e24e1c5eb8cc4
zodplugin <60656929+zodplugin@users.noreply.github.com>
2022-04-12 11:47:42 +0700

    Create petprelab.java

---
+++ Pemrograman Berbasis Objek/topik 3/petprelab.java
@@ -0,0 +1,28 @@
+public class Pet
+{
+    private String nama,hewan;
+    private int umur;
+    
+    public void setNama(String name){
+        this.nama = name;
+    }
+    
+    public void setHewan(String name){
+        this.hewan = name;
+    }
+    
+    public void setUmur(int umur){
+        this.umur = umur;
+    }
+    
+    public String getNama(){
+        return nama;
+    }
+    public String getHewan(){
+        return hewan;
+    }
+    public int getUmur(){
+        return umur;
+    }
+    
+}
