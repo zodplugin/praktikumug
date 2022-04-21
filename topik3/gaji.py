0e5c6dc98404811c0402fdc68ec87dadfa42f6a6
Muhammad Fadhlan Aqila <60656929+zodplugin@users.noreply.github.com>
2022-04-21 21:11:55 +0700

    Create gaji.py

---
+++ topik3/gaji.py
@@ -0,0 +1,12 @@
+def main():
+    gaji = int(input("Masukkan gaji Anda: "))
+    lama = int(input("Lama bekerja (tahun): "))
+    if (gaji >= 3000000 and lama >= 2):
+        print("Anda terkualifikasi untuk menerima pinjaman!")
+    else:
+        print("Anda tidak terkualifikasi untuk menerima pinjaman!")
+    
+    
+
+    
+main()
