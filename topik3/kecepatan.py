6b2acf51c0e0469deaf994c11443215f773d581d
Muhammad Fadhlan Aqila <60656929+zodplugin@users.noreply.github.com>
2022-04-21 21:03:55 +0700

    Create kecepatan.py

---
+++ topik3/kecepatan.py
@@ -0,0 +1,6 @@
+def cek_kecepatan(kecepatan):
+    # Tuliskan kode Anda di bawah.
+    if( kecepatan >= 20 and kecepatan <= 60 ):
+        print("Kecepatan normal")
+    else:
+        print("Kecepatan tidak normal")
