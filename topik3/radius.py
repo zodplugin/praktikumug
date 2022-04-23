91ecf2264b2c048044e1114e3f1c689a8ab6243c
Muhammad Fadhlan Aqila <60656929+zodplugin@users.noreply.github.com>
2022-04-23 20:21:09 +0700

    Update radius.py

--- topik3/radius.py
+++ topik3/radius.py
@@ -1,7 +1,21 @@
 import math
 
-radius = float(input("Masukkan radius lingkaran (cm): "))
-l = math.pi * radius * radius
-k = 2 * math.pi * radius
-print(f"Luas lingkaran dengan radius {radius:.2f} =  {l:.2f} cm2")
-print(f"Keliling lingkaran dengan radius {radius:.2f} = {k:.2f} cm")
+# Fungsi luas_lingkaran
+def luas_lingkaran(radius):
+    l = math.pi * radius * radius
+    return l
+    
+# Fungsi keliling_lingkaran():
+def keliling_lingkaran(radius):
+    k = 2 * math.pi * radius
+    return k
+
+# Fungsi main
+def main():
+    radius = float(input("Masukkan radius lingkaran (cm): "))
+    print(f"Luas lingkaran dengan radius {radius:.2f} =  {luas_lingkaran(radius):.2f} cm2")
+    print(f"Keliling lingkaran dengan radius {radius:.2f} = {keliling_lingkaran(radius):.2f} cm")
+    
+# Panggil fungsi main
+main()
+    
