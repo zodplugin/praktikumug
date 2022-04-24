0e9d855039ba14db2714edc3b3ffe664879e99d8
Muhammad Fadhlan Aqila <60656929+zodplugin@users.noreply.github.com>
2022-04-24 23:16:13 +0700

    Create panjang.py

---
+++ topik2/panjang.py
@@ -0,0 +1,3 @@
+panjang = int(input("Masukkan panjang (cm): "))
+lebar = int(input("Masukkan lebar (cm): "))
+print(f"Persegi panjang dengan panjang = {panjang} cm dan lebar = {lebar} cm mempunyai luas = {panjang*lebar} cm2 dan keliling = {2*(panjang+lebar)} cm.")
