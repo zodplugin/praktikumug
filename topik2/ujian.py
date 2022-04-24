0bb5e6ff20936abd40571c35f36f8d235bd655ad
Muhammad Fadhlan Aqila <60656929+zodplugin@users.noreply.github.com>
2022-04-24 23:14:22 +0700

    Create ujian.py

---
+++ topik2/ujian.py
@@ -0,0 +1,4 @@
+angka1 = int(input("Masukkan nilai ujian 1: "))
+angka2 = int(input("Masukkan nilai ujian 2: "))
+angka3 = int(input("Masukkan nilai ujian 3: "))
+print(f"Rata-rata nilai ujian: {(angka1+angka2+angka3)/3:.2f}")
