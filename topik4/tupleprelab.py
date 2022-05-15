64055152cb0e2da0c16fbeb74f0f28bd1b2b7815
Muhammad Fadhlan Aqila <60656929+zodplugin@users.noreply.github.com>
2022-05-15 08:35:54 +0700

    Create tupleprelab.py

---
+++ topik4/tupleprelab.py
@@ -0,0 +1,10 @@
+def main():
+    
+    nama_mhs = ('Budi', 'Dodi', 'Cindi', 'Kiki', 'Beti')
+    
+    # Tuliskan kode Anda di bawah.
+    for i in nama_mhs:
+        print(i)
+    
+# Panggil fungsi main
+main()
