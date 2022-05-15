0773207cf9bd60d52892f4b69f75c3b5b2d110a1
Muhammad Fadhlan Aqila <60656929+zodplugin@users.noreply.github.com>
2022-05-15 09:00:10 +0700

    Create inisial_nama.py

---
+++ topik4/inisial_nama.py
@@ -0,0 +1,14 @@
+def main():
+    # Tuliskan kode Anda di bawah dengan mengikuti petunjuk-petunjuk di bawah
+    
+    # [1] Minta pengguna memasukkan sebuah nama
+    namadepan = input('Masukkan nama: ')
+    namasplit = namadepan.split()
+    print(f'Inisial nama Anda: {(namasplit[0][0])}. {(namasplit[1][0])}.')
+    # [3] Tampilkan inisial nama depan dan nama belakang dengan operasi pengindeksan list dan string
+    # Anda dapat membayangkan list yang berisi string sebagai nested list.
+    
+
+# Panggil fungsi main
+main()
+    
